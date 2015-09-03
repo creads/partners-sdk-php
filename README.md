d4a93d9277c726b344eefaa9e756161065494458
pitpit <damien.pitard@gmail.com>
2015-09-03 23:39:07 +0200

    added CLI

--- README.md
+++ README.md
@@ -1,3 +1,120 @@
 # partners-api-php
 
-A simple PHP client for Creads Partners API
+A simple PHP client and CLI for Creads Partners API.
+
+## Installing Partners API PHP
+
+The recommended way to install Guzzle is through
+[Composer](http://getcomposer.org).
+
+Install Composer:
+
+```bash
+curl -sS https://getcomposer.org/installer | php
+```
+
+Run the Composer command to install the latest stable version:
+
+```bash
+composer.phar require creads/partners-api
+```
+
+## Using the library
+
+After installing, you need to require Composer's autoloader:
+
+```php
+require 'vendor/autoload.php';
+```
+
+### Get an OAuth2 token
+
+First you need to get a fresh OAuth2 access token:
+
+...
+ 
+###  How to
+
+Instance a new client with the token:
+
+```php
+use Creads\Partners\Client;
+
+$client = new Client($token);
+```
+
+Get information about the API:
+
+```php
+$root = $client->get('/');
+echo $root['version'];
+//1.0.0
+```
+
+Get information about me:
+
+```php
+$me = $client->get('/me');
+echo $me['firstname'];
+//John
+```
+
+Update my firstname:
+
+```php
+$client->put('/me', [
+    'firstname' => 'John'
+]);
+```
+
+Create a project:
+
+```php
+$client->post('/projects', [
+	'title' => '',
+	'description' => '',
+	'organization' => '',
+    'firstname' => 'John',
+    'product' => ''
+    'price' => ''
+]);
+``
+
+### Errors and exceptions handling
+
+When HTTP errors occurs, the library throws an `\Creads\Partners\HttpException` object:
+
+```php
+use Creads\Partners\HttpException;
+
+try {
+	$me = $client->get('/me');
+	echo $me['firstname'];
+} catch (HttpException) {
+    if (401 == $response->getStatusCode()) {
+        //do something
+    }
+}
+```
+
+If you prefer the client to do something on any http errors:
+
+```php
+$client->error(function(Response $response) {
+    if (401 == $response->getStatusCode()) {
+        //do something
+    }
+});
+```
+
+> Other non HTTP exceptions will continue to be thrown except if you want to globally catch them with:
+
+```php
+use Creads\Partners\RuntimeException;
+
+$client->exception(function(RuntimeException $exception) {
+	if ($exception instance)
+});
+```
+
+## Using the CLI application
---
+++ src/Command/LoginCommand.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Creads\Partners\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class LoginCommand extends Command
+{
+	protected function configure()
+    {
+        $this
+            ->setName('login')
+            ->setDescription('Log onto the API')
+            ->addOption(
+               'reset',
+               null,
+               InputOption::VALUE_NONE,
+               'Reset the client credentials'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dialog = $this->getHelperSet()->get('dialog');
+
+        //build the configuration file path
+        if (isset($_SERVER['HOME'])) {
+            $path = $_SERVER['HOME'];
+        } else {
+            $output->writeln('CLI failed to locate your home directory. Configuration file will be saved in current directory as `.partners.json`.');
+            $path = getcwd(); 
+        }
+        $path = $path.'/.partners.json';
+
+        if (!file_exists($path) || $input->getOption('reset', false)) {
+
+            //if there is no configuration file or if user passed --reset option
+            $config = [];
+            if (!$config['client_id'] = $dialog->ask(
+                $output,
+                '<question>Your client ID:</question> ',
+                false
+            )) {
+                return 1;
+            }
+
+            if (!$config['client_secret'] = $dialog->ask(
+                $output,
+                '<question>Your client secret:</question> ',
+                false
+            )) {
+                return 1;
+            }
+        } else {
+            //load the configuration
+            $config = json_decode(file_get_contents($path), true);
+            if (!$config) {
+                throw new \Exception(sprintf('Failed to load configuration file. Please run the command again with "--reset" option. If the problem persists, remove manually the file "%s".', $path));
+            }
+        }
+
+        $baseUri = 'https://connect-preprod.creads-partners.com';
+        $client = new \GuzzleHttp\Client(['base_uri' => $baseUri]);
+        try {
+            $response = $client->post('/oauth2/token', [
+                'auth' => [$config['client_id'], $config['client_secret']],
+                'form_params' => [
+                    'grant_type' => 'password', //@todo change for client_credentials
+                    'scope' => 'base',
+                    'username' => 'd.pitard@creads.org', //@todo remove
+                    'password' => 'br152qT3',
+                ]
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $data = json_decode($e->getResponse()->getBody(), true);
+            $message = isset($data['error_description'])?$data['error_description']:($e->getResponse()->getStatusCode() . ' ' . $e->getResponse()->getReasonPhrase());
+            throw new \Exception($message);
+        }
+
+        if (!($token = json_decode($response->getBody(), true)) || !isset($token['access_token'])) {
+            throw new \Exception('Failed to decode API response', $path);
+        }
+        $config['token'] = $token['access_token'];
+
+        //save the config
+        if (false === file_put_contents($path, json_encode($config, JSON_PRETTY_PRINT))) {
+            throw new \Exception(sprintf('Failed to store configuration file "%s". Can not continue.', $path));
+        }
+
+        $output->writeln('OK');
+    }
+}
