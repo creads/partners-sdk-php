d2e060fd36fd51dc4d14456c83eeca5e56998bdb
pitpit <damien.pitard@gmail.com>
2015-09-05 00:45:21 +0200

    login command

--- src/Command/LoginCommand.php
+++ src/Command/LoginCommand.php
@@ -10,16 +10,22 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class LoginCommand extends Command
 {
-	protected function configure()
+    protected function configure()
     {
         $this
             ->setName('login')
             ->setDescription('Log onto the API')
             ->addOption(
                'reset',
-               null,
+               'r',
                InputOption::VALUE_NONE,
-               'Reset the client credentials'
+               'Reset credentials'
+            )
+            ->addOption(
+                '--no-password',
+                null,
+                InputOption::VALUE_NONE,
+                'User credentials needed'
             )
         ;
     }
@@ -27,6 +33,8 @@ class LoginCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $dialog = $this->getHelperSet()->get('dialog');
+        $reset = $input->getOption('reset', false);
+        $noPassword = $input->getOption('no-password', false);
 
         //build the configuration file path
         if (isset($_SERVER['HOME'])) {
@@ -37,26 +45,7 @@ class LoginCommand extends Command
         }
         $path = $path.'/.partners.json';
 
-        if (!file_exists($path) || $input->getOption('reset', false)) {
-
-            //if there is no configuration file or if user passed --reset option
-            $config = [];
-            if (!$config['client_id'] = $dialog->ask(
-                $output,
-                '<question>Your client ID:</question> ',
-                false
-            )) {
-                return 1;
-            }
-
-            if (!$config['client_secret'] = $dialog->ask(
-                $output,
-                '<question>Your client secret:</question> ',
-                false
-            )) {
-                return 1;
-            }
-        } else {
+        if (file_exists($path)) {
             //load the configuration
             $config = json_decode(file_get_contents($path), true);
             if (!$config) {
@@ -64,17 +53,84 @@ class LoginCommand extends Command
             }
         }
 
+        if (!file_exists($path) || $reset) {
+
+            if (!$clientId = $dialog->ask(
+                $output,
+                sprintf('<question>Client ID</question>%s: ', $reset?(' [<comment>'.$config['client_id'].'</comment>]'):''),
+                false
+            )) {
+                if ($reset) {
+                    $clientId = $config['client_id'];
+                } else {
+                    throw new \InvalidArgumentException('Client ID is required');
+                }
+            }
+            $config['client_id'] = $clientId;
+
+            if (!$clientSecret = $dialog->ask(
+                $output,
+                sprintf('<question>Client Secret</question>%s: ', $reset?(' [<comment>'.$config['client_secret'].'</comment>]'):''),
+                false
+            )) {
+                if ($reset) {
+                    $clientSecret = $config['client_secret'];
+                } else {
+                    throw new \InvalidArgumentException('Client Secret is required');
+                }
+            }
+            $config['client_secret'] = $clientSecret;
+
+            if (!$noPassword) {
+                if (!$username = $dialog->ask(
+                    $output,
+                    sprintf('<question>Username</question>%s: ', ($reset && isset($config['username']))?(' [<comment>'.$config['username'].'</comment>]'):''),
+                    false
+                )) {
+                    if ($reset && isset($config['username'])) {
+                        $username = $config['username'];
+                    } else {
+                        throw new \InvalidArgumentException('Username is required');
+                    }
+                }
+                $config['username'] = $username;
+
+                $config['grant_type'] = 'password';
+            }
+        }
+
+        //even if we are not reseting credentials, we can switch to "no password" mode
+        if ($noPassword) {
+            $config['grant_type'] = 'client_credentials';
+        }
+
+        $params = [
+            'grant_type' => $config['grant_type'],
+            'scope' => 'base'
+        ];
+
+        if ('password' === $config['grant_type']) {
+
+            if (!$password = $dialog->askHiddenResponse(
+                $output,
+                '<question>Password</question>: ',
+                false
+            )) {
+                return 1;
+            }
+
+            $params = array_merge($params,[
+                'username' => $config['username'],
+                'password' => $password,
+            ]);
+        }
+
         $baseUri = 'https://connect-preprod.creads-partners.com';
         $client = new \GuzzleHttp\Client(['base_uri' => $baseUri]);
         try {
             $response = $client->post('/oauth2/token', [
                 'auth' => [$config['client_id'], $config['client_secret']],
-                'form_params' => [
-                    'grant_type' => 'password', //@todo change for client_credentials
-                    'scope' => 'base',
-                    'username' => 'd.pitard@creads.org', //@todo remove
-                    'password' => 'br152qT3',
-                ]
+                'form_params' => $params
             ]);
         } catch (\GuzzleHttp\Exception\ClientException $e) {
             $data = json_decode($e->getResponse()->getBody(), true);
